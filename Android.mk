281b87dee91cc10de5ee81898dd8dcfa3a1b27e6
Anonymous <none@none.none>
2020-03-23 16:21:46 +0100

    Convert to Android.bp
    Points of contention:
    - android/1.x is untouched
    - Should GNSS_HIDL_LEGACY_MEASURMENTS be applied for sm8150?
      Check everything regarding the statements in
      build/target_specific_features.mk
    - LOC_HIDL_VERSION is hardcoded to 2.0
    - Reliance on fwk-detect is stupid and requires importing
      the core-utils namespace, to be done in a later commit

--- Android.mk
+++ Android.mk
@@ -1,5 +1,2 @@
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-LOCAL_PATH := $(call my-dir)
-include $(LOCAL_PATH)/build/target_specific_features.mk
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
+# Empty file as build guard
+# Logic is in Android.bp files
